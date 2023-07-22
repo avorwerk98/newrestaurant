e7ea78dfb2de1c2e4582607b3dd94ead17a406c6
avery <avorwerk@bu.edu>
2023-07-22 13:30:19 -0400

    update

--- frontend/components/RestaurantList.jsx
+++ frontend/components/RestaurantList.jsx
@@ -70,7 +70,7 @@ function RestaurantCard({ data }) {
           <div className="flex flex-wrap md:justify-center -m-2">
             <div className="w-full md:w-auto p-2 my-6">
               <Link
-                className="block w-full px-12 py-3.5 text-lg text-center text-white font-bold bg-sky-500 hover:bg-emerald-500 focus:ring-4 focus:ring-gray-600 rounded-full"
+                className="block w-full px-12 py-3.5 text-lg text-center text-white font-bold bg-sky-560 hover:bg-emerald-500 focus:ring-4 focus:ring-gray-600 rounded-full"
                 href={`/restaurant/${data.id}`}
               >
                 Order
