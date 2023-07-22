677108db44341ce206a1be54b542b9842e0e5a35
avery <avorwerk@bu.edu>
2023-07-22 13:08:17 -0400

    images

--- frontend/components/RestaurantList.jsx
+++ frontend/components/RestaurantList.jsx
@@ -39,8 +39,8 @@ function RestaurantCard({ data }) {
             className="w-full rounded-2xl"
             height={300}
             width={300}
-            src={`${process.env.STRAPI_URL || "https://strapi-lh2y.onrender.com"}${imageUrl}`}
-            alt=""
+            src={imageUrl}
+            alt={data.attributes.name}
           />
         <div className="p-8">
           <h3 className="mb-3 font-heading text-xl text-indigo-900 hover:text-sky-700 group-hover:underline font-black">
@@ -52,7 +52,7 @@ function RestaurantCard({ data }) {
           <div className="flex flex-wrap md:justify-center -m-2">
             <div className="w-full md:w-auto p-2 my-6">
               <Link
-                className="block w-full px-12 py-3.5 text-lg text-center text-white font-bold bg-gradient-to-r from-indigo-500 from 20% via-sky-500 via 60% to-emerald-500 to-90% hover:bg-gradient-to-r from-indigo-600 from 20% via-sky-600 via 60% to-emerald-600 to-90% focus:ring-4 focus:ring-gray-600 rounded-full"
+                className="block w-full px-12 py-3.5 text-lg text-center text-white font-bold bg-sky-500 hover:bg-emerald-500 focus:ring-4 focus:ring-gray-600 rounded-full"
                 href={`/restaurant/${data.id}`}
               >
                 Order
